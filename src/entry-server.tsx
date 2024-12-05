c84781607b81831e239c536a28c36bd8759ad0fd
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2024-12-05 22:35:02 +0530

    test meta tags?

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -7,9 +7,28 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           <meta charset="utf-8" />
-          <title>www</title>
+          <title>rex's place</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="rex's personal website" />
+          <meta name="theme-color" content="#030303" />
+
+          <meta name="robots" content="index,follow" />
+          <meta name="googlebot" content="index,follow" />
+
+          <meta property="og:title" content="rex's place" />
+          <meta property="og:description" content="rex's personal website" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://rex.wf" />
+          <meta property="og:image" content="/image.png" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="rex's place" />
+          <meta name="twitter:description" content="rex's personal website" />
+          <meta name="twitter:image" content="/image.png" />
+
           <link rel="icon" href="/image.png" />
+          <link rel="apple-touch-icon" href="/image.png" />
+
           {assets}
         </head>
         <body>
