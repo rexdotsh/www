74d1f70562e39180c580ac426784a108fa919608
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2024-12-06 04:22:01 +0530

    what is going on

--- src/components/Art/index.tsx
+++ src/components/Art/index.tsx
@@ -8,9 +8,14 @@ const AsciiArt: Component<AsciiArtProps> = (props) => {
 
   return (
     <div class="h-full flex justify-center flex-col items-center">
+      {/* very weird bug that i cannot figure out, it is very dull on mobile, but fine on PC, even through devtools.
+      if i set it app.tsx to `fixed`, it becomes dull on pc too???? idk, maybe this shit needs a re-write
+      as a workaround, using text-rose-500 on mobile and text-rose-400/80 on pc. 
+      looks atrocious on mobile through devtools, but looks fine on my actual phone. wtf
+       */}
       <pre
         class="select-none text-[2.8px] md:text-[3.2px] leading-[1.4px] md:leading-[1.6px] 
-            text-rose-400/80 text-center mx-auto">
+            text-rose-500 md:text-rose-400/80 text-center mx-auto">
         {art()}
       </pre>
       <Navigation />
