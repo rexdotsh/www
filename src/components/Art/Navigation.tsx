4a139751995bc0ce06ec9a6b80daf9347d9cbe2c
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2024-12-08 02:04:27 +0530

    fix: make text color match blog

--- src/components/Art/Navigation.tsx
+++ src/components/Art/Navigation.tsx
@@ -11,7 +11,7 @@ const Navigation: Component = () => {
   return (
     <nav class="flex flex-col md:flex-row items-center gap-6 md:gap-12 mt-16 md:mt-12">
       {links.map(({href, text}) => (
-        <a href={href} class="text-lg md:text-2xl md:my-12 text-white font-bold hover:text-rose-400/80 transition-colors">
+        <a href={href} class="text-lg md:text-2xl md:my-12 text-[#d6e9ef] font-bold hover:text-rose-400/80 transition-colors">
           {text}
         </a>
       ))}
