207ec13555b40446d95b85aecb5db7f41ea22481
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-02-06 00:58:33 +0530

    feat: add target and rel attributes to navigation links

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -13,7 +13,7 @@ const Navigation: Component = () => {
     <nav class="flex flex-col md:flex-row items-center gap-6 md:gap-12 mt-8 mb-36 md:mt-12 md:mb-0">
       <For each={links}>
         {({ href, text }) => (
-          <a href={href} class="text-lg md:text-2xl md:my-12 text-[#d6e9ef] font-bold hover:text-rose-400/80 transition-colors">
+          <a href={href} target="_blank" rel="noopener noreferrer" class="text-lg md:text-2xl md:my-12 text-[#d6e9ef] font-bold hover:text-rose-400/80 transition-colors">
             {text}
           </a>
         )}
