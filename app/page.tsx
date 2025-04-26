f66d20eedf1214d6183a7ed0a0e9c82120189174
mridul <65942753+rexdotsh@users.noreply.github.com>
2025-04-27 00:50:31 +0530

    refactor: remove cringe

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import AsciiArt from '@/app/components/AsciiArt';
 import Footer from '@/app/components/Footer';
-import LocStats from '@/app/components/LocStats';
 import SpotifyNowPlaying from '@/app/components/Spotify';
 
 export default function Home() {
   return (
     <main className="fixed inset-0 overflow-hidden">
-      <LocStats />
       <AsciiArt />
       <SpotifyNowPlaying />
       <Footer />
