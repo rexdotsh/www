522ac2113035314693296f4d61405e6d726c3ec4
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-06-30 02:43:00 +0530

    fix: better rendering of ascii art on mobile devices?

--- app/components/AsciiArt/index.tsx
+++ app/components/AsciiArt/index.tsx
@@ -4,7 +4,13 @@ import { asciiArt } from './ascii-art';
 const AsciiArt = () => {
   return (
     <div className="h-full flex justify-center flex-col items-center">
-      <pre className="select-none text-[2.8px] md:text-[3.2px] leading-[1.4px] md:leading-[1.6px] text-rose-500 text-center mx-auto fade-in">
+      <pre 
+        className="select-none text-[2.8px] md:text-[3.2px] leading-[1.4px] md:leading-[1.6px] text-rose-500 text-center mx-auto fade-in"
+        style={{
+          whiteSpace: 'pre',
+          fontFamily: 'monospace, "Courier New", Courier'
+        }}
+      >
         {asciiArt}
       </pre>
       <div className="fade-in">
