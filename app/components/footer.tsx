637b4a2516fc6e21fd5261c36073b20fc880cc18
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-10-08 19:09:19 +0530

    chore: formatting

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
-  <footer className="absolute bottom-0 w-full px-6 py-2 text-center text-amber-800/70 text-sm md:py-4 md:text-base dark:text-neutral-500">
-    <div className="flex items-center justify-center space-x-3 transition-colors duration-300 hover:text-amber-950 dark:hover:text-neutral-300">
-      <span>© {new Date().getFullYear()}</span>
-      <span className="text-rose-700 dark:text-rose-400/80">•</span>
-      <span>rex@unreal:~#</span>
-    </div>
-  </footer>
+    <footer className="absolute bottom-0 w-full px-6 py-2 text-center text-amber-800/70 text-sm md:py-4 md:text-base dark:text-neutral-500">
+      <div className="flex items-center justify-center space-x-3 transition-colors duration-300 hover:text-amber-950 dark:hover:text-neutral-300">
+        <span>© {new Date().getFullYear()}</span>
+        <span className="text-rose-700 dark:text-rose-400/80">•</span>
+        <span>rex@unreal:~#</span>
+      </div>
+    </footer>
   );
 }
