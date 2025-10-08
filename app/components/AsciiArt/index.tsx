f824b37cae65f38695238f2de14493750260a92e
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-10-08 18:20:58 +0530

    fix: size on desktop

--- app/components/AsciiArt/index.tsx
+++ app/components/AsciiArt/index.tsx
@@ -3,7 +3,7 @@ import Navigation from "../navigation";
 
 const AsciiArt = () => (
   <div className="flex h-full flex-col items-center justify-center">
-    <div className="fade-in ml-6 w-full max-w-xs lg:mx-auto lg:max-w-sm">
+    <div className="fade-in ml-6 w-full max-w-xs lg:mx-auto lg:max-w-xs">
       <Image
         alt="Rose"
         className="h-auto w-full select-none"
