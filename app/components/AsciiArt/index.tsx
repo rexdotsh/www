2a2f696f1eb1c371a202a7f5916ba491eecb9383
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-10-08 17:51:01 +0530

    feat: switch to webp based generation

--- app/components/AsciiArt/index.tsx
+++ app/components/AsciiArt/index.tsx
@@ -1,18 +1,18 @@
+import Image from "next/image";
 import Navigation from "../navigation";
-import { asciiArt } from "./ascii-art";
 
 const AsciiArt = () => (
   <div className="flex h-full flex-col items-center justify-center">
-    <pre
-      className="fade-in mx-auto select-none text-center text-[2.8px] text-rose-500 leading-[0.5] md:text-[3.2px]"
-      style={{
-        whiteSpace: "pre",
-        fontFamily: 'monospace, "Courier New", Courier',
-        WebkitTextSizeAdjust: "100%",
-      }}
-    >
-      {asciiArt}
-    </pre>
+    <div className="fade-in mx-auto w-full max-w-[340px]">
+      <Image
+        alt="Rose"
+        className="h-auto w-full select-none"
+        height={370}
+        priority
+        src="/rose.webp"
+        width={370}
+      />
+    </div>
     <div className="fade-in">
       <Navigation />
     </div>
