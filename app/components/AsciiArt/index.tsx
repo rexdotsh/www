9f6cd0ab16cd4db7babf9b4d25e0cf7f4e2e1749
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-10-08 16:24:38 +0530

    fix: try to adjust webtextkitsize

--- app/components/AsciiArt/index.tsx
+++ app/components/AsciiArt/index.tsx
@@ -4,10 +4,11 @@ import { asciiArt } from "./ascii-art";
 const AsciiArt = () => (
   <div className="flex h-full flex-col items-center justify-center">
     <pre
-      className="fade-in mx-auto select-none text-center text-[2.8px] text-rose-500 leading-[1.4px] md:text-[3.2px] md:leading-[1.6px]"
+      className="fade-in mx-auto select-none text-center text-[2.8px] text-rose-500 leading-[0.5] md:text-[3.2px]"
       style={{
         whiteSpace: "pre",
         fontFamily: 'monospace, "Courier New", Courier',
+        WebkitTextSizeAdjust: "100%",
       }}
     >
       {asciiArt}
