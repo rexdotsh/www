7e2d409be33a7ff14a70fa12cf556e89c57643d1
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-10-08 20:17:37 +0530

    feat: new colors for light mode

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -16,7 +16,7 @@ export default async function Navigation() {
     <nav className="mt-6 mb-36 flex flex-col items-center gap-6 md:mt-8 md:mb-0 md:flex-row md:gap-12">
       {links.map(({ href, text }) => (
         <a
-          className="font-bold text-[var(--primary)] text-lg transition-colors hover:text-rose-700 md:my-12 md:text-2xl dark:hover:text-rose-500/80"
+          className="font-bold text-lg text-rose-700 transition-colors hover:text-rose-500 md:my-12 md:text-2xl dark:text-[var(--primary)] dark:hover:text-rose-500/80"
           href={href}
           key={href}
           rel="noopener noreferrer"
