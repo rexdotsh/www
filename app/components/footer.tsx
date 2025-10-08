766b66d0882bd83b9aad6bf1ca6e2235e8ffacdb
rexdotsh <65942753+rexdotsh@users.noreply.github.com>
2025-10-08 18:16:03 +0530

    feat: better footer alignment

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="absolute bottom-0 w-full px-6 py-4 text-center text-amber-800/70 text-sm md:text-base dark:text-neutral-500">
+  <footer className="absolute bottom-0 w-full px-6 py-2 text-center text-amber-800/70 text-sm md:py-4 md:text-base dark:text-neutral-500">
     <div className="flex items-center justify-center space-x-3 transition-colors duration-300 hover:text-amber-950 dark:hover:text-neutral-300">
       <span>© {new Date().getFullYear()}</span>
       <span className="text-rose-700 dark:text-rose-400/80">•</span>
